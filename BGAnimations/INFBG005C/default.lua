5d27d7561ab52b60b7f33f7088ec450852cb6c25
EngineMachiner <mfpc98@gmail.com>
2025-11-25 23:07:28 -0500

    Remove useless animation.

--- BGAnimations/INFBG005C/default.lua
+++ BGAnimations/INFBG005C/default.lua
@@ -5,10 +5,12 @@ local SongBG = Builder.SongBackground():merge { Colors = color("#707070") }
 
 return Builder.Load {
 
-    Texture = "Infinitesimal/lights 3x1.png",           Zoom = 0.3,
+    Texture = "Infinitesimal/lights 3x1.png",       Quantity = 4,
 
-    States = { Last = 3,    Types = { "Static", "Position" } },         Colors = "RainbowBlink",
+    States = { Last = 3,    Types = { "Idle", "Position" } },       Zoom = 3,       Rate = 2,
 
-    Script = "Particles/Depth/StaticScatter",          Layers = { Back = SongBG:Load() }
+    Script = "Particles/Depth/StaticScatter",          Layers = { Back = SongBG:Load() },
+
+    Sprite = { OnCommand=function(self) self:blend("BlendMode_InvertDest") end }
 
 }
