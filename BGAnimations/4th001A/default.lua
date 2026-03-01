1c864e0d940c1162f233cc9d6c7f161cd5d0b0b9
EngineMachiner <mfpc98@gmail.com>
2026-03-01 13:16:04 -0500

    4th001A states fix

--- BGAnimations/4th001A/default.lua
+++ BGAnimations/4th001A/default.lua
@@ -15,7 +15,7 @@ return Builder.Load {
 
         Dynamic = true,     Texture = "4th/1 1x4.png",      Blend = "add",
         
-        Matrix = Vector( 1, 3.75 ),        States = { Last = 4,    Types = "Random",   Rate = 0.5 },
+        Matrix = Vector( 1, 3.75 ),        States = { Last = 4,    Types = "Random" },
 
         Sprite = { OnCommand=function(self) local i = self.Index - 1        self:addy( i * 20 ) end }
         
