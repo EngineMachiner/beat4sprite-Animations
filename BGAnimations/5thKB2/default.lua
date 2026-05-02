f1fcaf195df4504ae5782599eccd86855bb5bd76
EngineMachiner <mfpc98@gmail.com>
2026-05-02 01:15:27 -0500

    Update default.lua

--- BGAnimations/5thKB2/default.lua
+++ BGAnimations/5thKB2/default.lua
@@ -1,17 +1,48 @@
 
-local params = beat4sprite.create( {
+local Vector = Astro.Vector         local componentDivision = Vector.componentDivision
 
-	{	
-		File = "5th/Backgrounds/A.png",
-		Script = "Kaleidoscopes/Comb.lua"
-	},
+local Renderer = tapLua.Sprite.Renderer         local Matrix = Vector( 5, 1 )
 
-	{
-		File = "5th/Sprites/X2 5x1.png",
-		Columns = 5,	firstState = 1,	lastState = 5,
-		Commands = { "StairsStates", "SpinX" }
-	}
-	
-} )
+local properties = tapLua.Load( "Sprite/Crop", "States" )           local p = properties(Matrix)
 
-return params:Load()
+local function size() return Renderer:GetSize() end
+
+local builder = beat4sprite.Builder.Retro {
+
+    { Texture = "5th/1.png",         Script = "Kaleidoscope/Tile" },
+
+    {
+
+        Dynamic = true,     Texture = "5th/2.png",      Matrix = Matrix,
+        
+        States = { Matrix = Matrix,     Scroll = Vector("Right"),      Position = true,    Rate = 0.25 },
+
+        Sprite = {
+
+            InitCommand=function(self)
+                
+                if self.Index > 1 then return end
+
+                local size = componentDivision( size(), Matrix )        Renderer:setSizeVector(size)
+            
+            end,
+
+            OnCommand=function(self)
+
+                self:setSizeVector( size() ):queuecommand("PostInit")
+
+                self:SetStateProperties(p)          local p = self:scrollStates()
+
+                self:SetStateProperties(p):queuecommand("Spin")
+            
+            end,
+
+            SpinCommand = beat4sprite.Actor.Commands.SpinX
+
+        }
+
+    }
+
+}
+
+return builder:merge(...):Load()
